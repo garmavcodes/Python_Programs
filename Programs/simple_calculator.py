bb37d6b6b874124a932fa3a9712b14416ed9d7c7
Garma Kumari <garmakumariprajapati@gmail.com>
2025-09-17 17:18:30 +0530

    added program

---
+++ Programs/simple_calculator.py
@@ -0,0 +1,10 @@
+# take two values from user and store in variables a and b
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+print("the sum of a and b:",a+b) #prints addition
+print("the sub of a and b:",a-b) #prints subtraction
+print("The mul of a and b:",a*b) #prints muliplication
+print("The div of a and b:",a/b) #prints division
+print("The mod of a and b:",a%b) #prints modulus
+print("The exp of a and b:",a**b) #prints exponentiation
+print("The floor of a and b:",a/b) #prints floor division
